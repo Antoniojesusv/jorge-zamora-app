11f06eaa536c63cca63b3220c8c446141d1b8799
Antonio Jesús Vázquez Muñoz <antoniojesusvazquezmunozz@gmail.com>
2020-05-20 13:38:18 +0200

    build: frontend app structure moved to top level

---
+++ src/carousel/position.service.js
@@ -0,0 +1,12 @@
+class Position {
+  constructor() {
+    this.say = '';
+  }
+
+  sayHello2() {
+    this.say = 'I am the position dependencie';
+    return this.say;
+  }
+}
+
+export default Position;
