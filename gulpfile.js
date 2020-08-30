91f90a0fd4be824892d093e650cf2bcf400479ee
Antoniojesusv <antoniojesusvazquezmunozz@gmail.com>
2020-08-30 18:51:44 +0200

    fix: fix watch task and browserSync config

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,12 @@ const babelify = require('babelify');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const log = require('gulplog');
-const uglify = require('gulp-uglify');
 const sourcemaps = require('gulp-sourcemaps');
 const assign = require('lodash.assign');
+const browserSync = require('browser-sync').create();
+const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
 const htmlmin = require('gulp-htmlmin');
 
 const customOpts = {
@@ -22,7 +25,7 @@ const b = watchify(
   })
 );
 
-const jsBundle = () => {
+const jsBundle = function () {
   return b
     .bundle()
     .on('error', log.error.bind(log, 'Browserify Error'))
@@ -31,28 +34,57 @@ const jsBundle = () => {
     .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(uglify())
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
 };
 
 b.on('update', jsBundle);
 b.on('log', log.info);
 
-const htmlMini = () => {
+const htmlMini = function () {
   return gulp
     .src('./frontend/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
 };
 
-exports.default = () => {
+const css = function () {
+  return gulp
+    .src('./frontend/**/*.css')
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
+};
+
+const browserSyncTask = function (done) {
+  browserSync.init({
+    proxy: 'http://xiwily4.local:8080/',
+  });
+  done();
+};
+
+const watchFiles = function () {
+  gulp.watch('./frontend/**/*.css', css);
   gulp.watch(
     [
       './frontend/*.js',
-      './frontend/**/*.js',
       './frontend/*.jsx',
+      './frontend/**/*.js',
       './frontend/**/*.jsx',
     ],
-    gulp.series(jsBundle)
+    jsBundle
   );
-  gulp.watch('./frontend/*.html', htmlMini);
+  gulp.watch(['./frontend/*.html', './frontend/**/*.html'], htmlMini);
 };
+
+const build = gulp.series(htmlMini, css, jsBundle);
+const watch = gulp.parallel(watchFiles, browserSyncTask);
+
+exports.watch = watch;
+exports.default = build;
